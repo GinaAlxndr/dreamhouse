982eb709bd8b3ee8197c7255c32c9ca8166acbca
gina khalisa <khalisa.gina18@gmail.com>
2021-09-29 12:24:56 +0700

    initialcommit

---
+++ app/src/main/java/com/example/dream_house/slide_8.java
@@ -0,0 +1,14 @@
+package com.example.dream_house;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class slide_8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide8);
+    }
+}
